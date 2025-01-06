68edc2f4ff6d25c1a93f6773578f7b6416d6e0a0
Valentin Weiss <valentin.weiss@hotmail.com>
2025-01-06 15:59:09 +0100

    feat: listen to incoming live events

--- src/hooks/useLatestEvents.ts
+++ src/hooks/useLatestEvents.ts
@@ -14,25 +14,24 @@
  * limitations under the License.
  */
 
-import { EventType, MatrixEvent, Room } from 'matrix-js-sdk';
+import { EventType, IRoomTimelineData, MatrixEvent, Room, RoomEvent } from 'matrix-js-sdk';
 import { useEffect, useState } from 'react';
 import { useMatrixClient } from './useMatrixClient';
 
 
 interface Props {
   rooms?: Room[];
-  eventTypes?: EventType[];
 }
 
 // Obtain the latest message event per room
 const useLatestEvents = ({
   rooms = [],
 }: Props = {}) => {
-  const { mx, cryptoApi } = useMatrixClient();
+  const { mx } = useMatrixClient();
   const [latestEvents, setLatestEvents] = useState<(MatrixEvent | undefined)[]>([]);
 
   useEffect(() => {
-    if (mx && cryptoApi) {
+    if (mx) {
       (async () => {
         const decryptionPromises = rooms.map((room) => room.decryptCriticalEvents());
         await Promise.all(decryptionPromises);
@@ -49,7 +48,35 @@ const useLatestEvents = ({
         setLatestEvents(_latestEvents);
       })();
     }
-  }, [mx, cryptoApi, rooms]);
+  }, [mx, rooms]);
+
+  useEffect(() => {
+    // update when a new live event arrives in one of the rooms
+    const updateLatestEvent = async (
+      event: MatrixEvent,
+      room: Room | undefined,
+      _toStartOfTimeline: boolean | undefined,
+      _removed: boolean | undefined,
+      data: IRoomTimelineData,
+    ) => {
+      if (data.liveEvent && room) {
+        await mx?.decryptEventIfNeeded(event);
+
+        if (EventType.RoomMessage === event.getType()) {
+          const index = rooms.findIndex((entry) => entry.roomId === room.roomId);
+          if (index > -1) {
+            latestEvents[index] = event;
+            setLatestEvents([...latestEvents]);
+          }
+        }
+      }
+    };
+
+    mx?.on(RoomEvent.Timeline, updateLatestEvent);
+    return () => {
+      mx?.removeListener(RoomEvent.Timeline, updateLatestEvent);
+    };
+  }, [mx, rooms, latestEvents]);
 
   return latestEvents;
 };
